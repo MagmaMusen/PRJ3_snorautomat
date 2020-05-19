68726f8f3d6563c04300e4956af41fcf3e0fbc56
Your Name <you@example.com>
2020-05-19 15:26:12 +0200

    Update to CDS

--- code/rpi/I2CVirtual/I2CVirtual.hpp
+++ code/rpi/I2CVirtual/I2CVirtual.hpp
@@ -7,26 +7,23 @@
 class i2c
 {
     public:
-    /*
-    *   @i2c()      Constructoren for I2C class. It opens the I2c protocol and IOCTL on the rpi
-    *           
-    *   @param[in]  adresse     Set the adress for the I2C protcol on the Rpi.
-    *         
-    */
+    
+    //! Costructoren for I2C class. It opens the I2c protocol and IOCTL on the Rpi
+    //! Set the address for the I2C protocol on the Rpi         
     i2c(int adresse);
-    /*
-    *   @void i2cSend()     It sends a char buffer from master to slave by I2C.
-    *   
-    */
+    
+    //! It sends a char buffer from master to slave by I2C. 
     void i2cSend(char buffer1);
-    /*
-    *   @char i2cReceive()      It returns a char buffer from slave to master by I2C.
-    *   
-    *   @param[out] buffer1     The buffer which is sent from master to slave
-    */
+
+//! It returns a char buffer from slave to master by I2C. 
+//! The buffer1 is the buffer which is sent from master to slave
     char i2cReceive();
 
     private:
+    
+//! The address of i2c slave 
     int adresse_;
+
+//! The buffer that is being send from master to slave
     char buffer_;
 };
